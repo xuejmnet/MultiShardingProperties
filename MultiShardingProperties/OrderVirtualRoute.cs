058d12c4ffcde862280a407e56daef41300999dc
xuejiaming <326308290@qq.com>
2021-12-26 17:01:02 +0800

    1

--- MultiShardingProperties/OrderVirtualRoute.cs
+++ MultiShardingProperties/OrderVirtualRoute.cs
@@ -8,7 +8,7 @@ using ShardingCore.VirtualRoutes.Months;
 
 namespace MultiShardingProperties
 {
-    public class OrderVirtualRoute:AbstractSimpleShardingMonthKeyDateTimeVirtualTableRoute<Order>
+    public class OrderVirtualRoute : AbstractSimpleShardingMonthKeyDateTimeVirtualTableRoute<Order>
     {
         /// <summary>
         /// 配置主分表字段是CreateTime,额外分表字段是OrderNo
@@ -60,7 +60,7 @@ namespace MultiShardingProperties
             ShardingOperatorEnum shardingOperator)
         {
             //将分表字段转成订单编号
-            var orderNo = shardingKey?.ToString()??string.Empty;
+            var orderNo = shardingKey?.ToString() ?? string.Empty;
             //判断订单编号是否是我们符合的格式
             if (!CheckOrderNo(orderNo, out var orderTime))
             {
@@ -69,46 +69,72 @@ namespace MultiShardingProperties
             }
 
             //当前时间的tail
-            var t = TimeFormatToTail(orderTime);
+            var currentTail = TimeFormatToTail(orderTime);
             //因为是按月分表所以获取下个月的时间判断id是否是在临界点创建的
             var nextMonthFirstDay = ShardingCoreHelper.GetNextMonthFirstDay(DateTime.Now);
             if (orderTime.AddSeconds(10) > nextMonthFirstDay)
             {
-                var nextT = TimeFormatToTail(nextMonthFirstDay);
-
-                if (shardingOperator == ShardingOperatorEnum.Equal)
-                {
-                    return tail => tail == t || tail == nextT;
-                }
+                var nextTail = TimeFormatToTail(nextMonthFirstDay);
+                return DoOrderNoFilter(shardingOperator, orderTime, currentTail, nextTail);
             }
             //因为是按月分表所以获取这个月月初的时间判断id是否是在临界点创建的
-            else if (orderTime.AddSeconds(-10) < ShardingCoreHelper.GetCurrentMonthFirstDay(DateTime.Now))
+            if (orderTime.AddSeconds(-10) < ShardingCoreHelper.GetCurrentMonthFirstDay(DateTime.Now))
             {
                 //上个月tail
-                var nextT = TimeFormatToTail(orderTime.AddSeconds(-10));
+                var previewTail = TimeFormatToTail(orderTime.AddSeconds(-10));
 
-                if (shardingOperator == ShardingOperatorEnum.Equal)
-                {
-                    return tail => tail == t || tail == nextT;
-                }
-            }
-            else
-            {
-                if (shardingOperator == ShardingOperatorEnum.Equal)
-                {
-                    return tail => tail == t;
-                }
+                return DoOrderNoFilter(shardingOperator, orderTime, previewTail, currentTail);
             }
 
-            return tail => true;
+            return DoOrderNoFilter(shardingOperator, orderTime, currentTail, currentTail);
+
         }
 
-        private bool CheckOrderNo(string orderNo,out DateTime orderTime)
+        private Expression<Func<string, bool>> DoOrderNoFilter(ShardingOperatorEnum shardingOperator, DateTime shardingKey, string minTail, string maxTail)
+        {
+            switch (shardingOperator)
+            {
+                case ShardingOperatorEnum.GreaterThan:
+                case ShardingOperatorEnum.GreaterThanOrEqual:
+                    {
+                        return tail => String.Compare(tail, minTail, StringComparison.Ordinal) >= 0;
+                    }
+
+                case ShardingOperatorEnum.LessThan:
+                    {
+                        var currentMonth = ShardingCoreHelper.GetCurrentMonthFirstDay(shardingKey);
+                        //处于临界值 o=>o.time < [2021-01-01 00:00:00] 尾巴20210101不应该被返回
+                        if (currentMonth == shardingKey)
+                            return tail => String.Compare(tail, maxTail, StringComparison.Ordinal) < 0;
+                        return tail => String.Compare(tail, maxTail, StringComparison.Ordinal) <= 0;
+                    }
+                case ShardingOperatorEnum.LessThanOrEqual:
+                    return tail => String.Compare(tail, maxTail, StringComparison.Ordinal) <= 0;
+                case ShardingOperatorEnum.Equal:
+                    {
+                        var isSame = minTail == maxTail;
+                        if (isSame)
+                        {
+                            return tail => tail == minTail;
+                        }
+                        else
+                        {
+                            return tail => tail == minTail || tail == maxTail;
+                        }
+                    }
+                default:
+                    {
+                        return tail => true;
+                    }
+            }
+        }
+
+        private bool CheckOrderNo(string orderNo, out DateTime orderTime)
         {
             //yyyyMMddHHmmss+new Random().Next(0,10000).ToString().PadLeft(4,'0')
             if (orderNo.Length == 18)
             {
-                if (DateTime.TryParseExact(orderNo.Substring(0,14), "yyyyMMddHHmmss", CultureInfo.InvariantCulture,
+                if (DateTime.TryParseExact(orderNo.Substring(0, 14), "yyyyMMddHHmmss", CultureInfo.InvariantCulture,
                         DateTimeStyles.None, out var parseDateTime))
                 {
                     orderTime = parseDateTime;
